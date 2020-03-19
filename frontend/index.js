9d4424c1ca46bce1f32de7b0f7cf768036f36436
Freddy Silber <freddy.silber@cloudcoach.com>
2020-03-19 11:15:30 -0600

    clean code

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,9 @@
 console.log('0.05', '-- IN JAVASCRIPT FILE')
-console.log('1. My project my life... im bout to destroy this bitch', 666)
+console.log('1. My pro ject my life... im bout to destroy this beotch', 666)
 const app1 = document.getElementById('#app')
-console.log('2. FUCKING APP', app1)
+console.log('2. chicken APP', app1)
 console.log('3. HOLY BRED')
-console.log('4. bitch')
+console.log('4. four chickens')
 let app
 const bruh = window.addEventListener('DOMContentLoaded', (event) => {
 	console.log('*** fsaf;dsjafdafdsafdsafdsafsa', event)
@@ -13,4 +13,4 @@ const bruh = window.addEventListener('DOMContentLoaded', (event) => {
 });
 console.log('APP', app)
 console.log(event)
-console.log('5. ASS')
+console.log('5. chicken sandwitch')
