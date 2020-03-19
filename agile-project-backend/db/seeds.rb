2979e1fd9e9b867b087b9e0dd4fbe5b48297d0d4
Freddy Silber <freddy.silber@cloudcoach.com>
2020-03-19 15:14:31 -0600

    filling seed file

--- agile-project-backend/db/seeds.rb
+++ agile-project-backend/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require 'securerandom'
+
+Project.delete_all 
+
+project_names = [
+	'Project1',
+	'while(1 < 2)',
+	'4x4=12',
+	'Another Project Name',
+	'POWER MODE'
+]
+
+project_names.each do |name|
+	Project.create(name: name)
+end
--- agile-project-frontend/index.js
+++ agile-project-frontend/index.js
@@ -1,16 +1,10 @@
 console.log('0.05', '-- IN JAVASCRIPT FILE')
-console.log('1. My pro ject my life... im bout to destroy this beotch', 666)
 const app1 = document.getElementById('#app')
 console.log('2. chicken APP', app1)
-console.log('3. HOLY BRED')
-console.log('4. four chickens')
 let app
 const bruh = window.addEventListener('DOMContentLoaded', (event) => {
-	console.log('*** fsaf;dsjafdafdsafdsafdsafsa', event)
-	console.log('6. DOM fully loaded and parsed', 'YEEEEEEEAAAAAHHH!!!!!!!', event);
+	console.log('6. DOM fully loaded and parsed', 'YEEEEEEEAAAAAHHH!!!!!!!', event)
 	app = document.getElementById('app')
 	console.log('6.1 --', app)
-});
-console.log('APP', app)
-console.log(event)
-console.log('5. chicken sandwitch')
+})
+console.log('APP', app, event)
