660f6af8b06d1a262db54bc24a557bc453bc6daa
Freddy Silber <freddy.silber@cloudcoach.com>
2020-03-19 16:30:43 -0600

    removing comments

--- agile-project-backend/app/serializers/project_serializer.rb
+++ agile-project-backend/app/serializers/project_serializer.rb
@@ -1,12 +1,4 @@
 class ProjectSerializer 
 	include FastJsonapi::ObjectSerializer
 	attributes :name
-# class ProjectSerializer < ActiveModel::Serializer
-	# def initialize(project_object)
-	# 	@project = project_object
-	# end
-
-	# def to_serialized_json
-	# 	@project.to_json(include: [:name])
-	# end
 end
