bd044106080d1da92653cdb5125a6a1eb071a654
Freddy Silber <freddy.silber@cloudcoach.com>
2020-03-20 12:57:18 -0600

    add gitignore and notes

--- agile-project-backend/config/routes.rb
+++ agile-project-backend/config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :projects
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/test', to: 'application#test'
+  scope :api, defaults: {format: :json} do
+    resources :projects
+  end
 end
--- agile-project-frontend/src/index.js
+++ agile-project-frontend/src/index.js
@@ -1,44 +1,14 @@
 const ROOT_URL = 'http://localhost:3000'
 const PROJECTS_INDEX = `${ROOT_URL}/projects`
-let PROJECT_DAT
 
 window.addEventListener("DOMContentLoaded", () => {
 	console.log('test')
-	// getProjects()
-	// getProjects = () => {
 	fetch(PROJECTS_INDEX)
 		.then(response => response.json())
 		.then(object => {
 			console.log(object)
-			PROJECT_DATA = object
 		})
 		.catch((error) => {
 			console.log('<ERROR>', error)
 		})
-	// }
-	console.log(projectData())
-	getProjects().then(data => console.log(data))
-
 })
-
-// getProjects = () => {
-// 	fetch(PROJECTS_INDEX)
-// 		.then(response => response.json())
-// 		.then(object => {
-// 			console.log(object)
-// 		})
-// } 
-
-const projectData = () => {
-	return fetch(`${PROJECTS_INDEX}/test`)
-		.then(response => response.json())
-		.then(parsed = console.log(parsed))
-}
-
-async function getProjects() {
-	let response = await fetch(`${PROJECTS_INDEX}/test`)
-	let data = await response.json()
-	return data
-}
-
-getProjects().then(data => console.log(data))
