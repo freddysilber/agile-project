6010f404297b9cabf9391108edefd83b4920935e
Freddy Silber <freddy.silber@cloudcoach.com>
2020-03-20 10:06:08 -0600

    learning how to set this up for rendering

--- agile-project-frontend/src/index.js
+++ agile-project-frontend/src/index.js
@@ -1,15 +1,35 @@
 const ROOT_URL = 'http://localhost:3000'
 const PROJECTS_INDEX = `${ROOT_URL}/projects`
+let PROJECT_DAT
 
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", () => {
 	console.log('test')
-	getProjects()
-})
-
-getProjects = () => {
+	// getProjects()
+	// getProjects = () => {
 	fetch(PROJECTS_INDEX)
 		.then(response => response.json())
 		.then(object => {
 			console.log(object)
+			PROJECT_DATA = object
 		})
+		.catch((error) => {
+			console.log('<ERROR>', error)
+		})
+	// }
+	console.log(projectData)
+})
+
+// getProjects = () => {
+// 	fetch(PROJECTS_INDEX)
+// 		.then(response => response.json())
+// 		.then(object => {
+// 			console.log(object)
+// 		})
+// } 
+
+const projectData = () => {
+	return fetch(`${PROJECTS_INDEX}/test`)
+		.then(response => response.json())
+		.then(parsed = console.log(parsed))
+
 }
