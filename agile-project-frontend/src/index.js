eb3706ba0735d74695594eb8335096d877dae0d9
Freddy Silber <freddy.silber@cloudcoach.com>
2020-03-20 14:01:01 -0600

    creating card item functions

--- agile-project-frontend/src/index.js
+++ agile-project-frontend/src/index.js
@@ -1,14 +1,42 @@
 const ROOT_URL = 'http://localhost:3000'
 const PROJECTS_INDEX = `${ROOT_URL}/projects`
+let APP
 
 window.addEventListener("DOMContentLoaded", () => {
-	console.log('test')
-	fetch(PROJECTS_INDEX)
-		.then(response => response.json())
-		.then(object => {
-			console.log(object)
+	APP = document.getElementById('app')
+	getProjects().then(data => {
+		const projects = createProjectCards(data.data)
+		projects.forEach(project => {
+			const cardItem = `
+				<div class="projectCard">
+					<p>${project.id}</p>
+					<p>${project.name}</p>
+				</div>
+			`
+			APP.insertAdjacentHTML('beforeend', cardItem)
 		})
-		.catch((error) => {
-			console.log('<ERROR>', error)
-		})
+	})
+
 })
+
+async function getProjects() {
+	let response = await fetch(PROJECTS_INDEX)
+	let data = await response.json()
+	return data
+}
+
+const createProjectCards = (data) => {
+	let projects = []
+	data.forEach(project => {
+		const p = new Project(project.id, project.attributes.name)
+		projects.push(p)
+	})
+	return projects
+}
+
+class Project {
+	constructor(id, name) {
+		this.id = id
+		this.name = name
+	}
+}
