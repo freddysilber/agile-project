06efd6340e4d5af04e333f9147cfe4ad284c3fc8
freddysilber <freddy.silber@cloudcoach.com>
2020-03-22 13:11:56 -0600

    added task resource

--- agile-project-backend/db/schema.rb
+++ agile-project-backend/db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_210540) do
+ActiveRecord::Schema.define(version: 2020_03_22_191128) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
